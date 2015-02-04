6ae69a571b1266c81dbd8081b44a3f887035c662
mbax4dh4 <daniel.hodgson-2@student.manchester.ac.uk>
2015-02-04 15:27:46 +0000

    Added stub for the language class.

---
+++ inc/classes/language.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The Language class, for the programmers' ease.
+ *
+ * No instances should be made of this class.
+ */
+abstract class Language
+{
+  static const ENGLISH  = 1;
+  static const ROMANIAN = 2;
+}
+?>
