43e824f24571ff1cbce7c5b1788f79037a5eefe6
Alex Radu <dragos.radu@student.manchester.ac.uk>
2015-02-10 23:50:50 +0000

    Added messages index. Added Message class. Added rmessages table.

---
+++ inc/classes/message.php
@@ -0,0 +1,102 @@
+<?php
+/**
+* Class Message
+*
+* Represents a message sent by an user
+*
+*/
+
+class Message
+{
+  // The text of the message
+  private $text
+  // The time and date of the message as dd-mm-yyyy-hh-mm-ss
+  private $timeStamp
+  // The unique id of the message
+  private $id
+  // The id of the user that has written the message
+  private $author
+  // The db connection handler
+  private $con
+
+  /**
+  *
+  * Constructor
+  *
+  * Constructs a message, given its id OR its text
+  *
+  * @param - $key(String), can be either 'id' or 'text', and determines if the message is 
+  *         just retrieved from db, or if a new message is inserted
+  * @param - $values(mixed), the id or the text and userIds, as array, of the message
+  */
+  public function __construct($con, $key, $values)
+  {
+    if($key == 'id')
+    {
+      // Localise the id
+      $message_id = $values;
+
+      // Get the message values from db
+      $stmt = $con->prepare("SELECT * FROM rmessages WHERE message_id = $message_id");
+      $stmt->execute();
+      $messageDetails = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      // Assign instance variables
+      $this->text = $messageDetails['message_text'];
+      $this->author = $messageDetails['message_user_id1'];
+      $this->con = $con;
+      $this->timeStamp = $messageDetails['message_timestamp'];
+    }
+    else if($key == 'text')
+    {
+      // Localise the text
+      $message_text = $values[2];
+      $message_user_id1 = $values[0];
+      $message_user_id2 = $values[1];
+
+      // Insert new message in db
+      $stmt = $con->prepare("INSERT INTO rmessages (message_text, message_user_id1, message_user_id2)
+                              VALUES ('$message_text', $message_user_id1, $message_user_id2)");
+      $stmt->execute();
+
+      // Assign instance variables
+      $this->text = $message_text;
+      $this->author = $message_user_id1;
+      $this->con = $con;
+      $this->timeStamp = date('d-m-Y-H-i-s');
+    }
+  }
+
+  /**
+  * Function getDetails($key)
+  *
+  * Returns id,author,text or timestamp, depending on the values of $key
+  *
+  * @param - $key(String), can be 'id', 'author', 'text' or 'timestamp'
+  * @return - $detail(mixed), the detail returned
+  */
+  public function getDetails($key)
+  {
+    switch ($key)
+    {
+      case 'id':
+        return $this->id;
+        break;
+      case 'author':
+        return $this->author;
+        break;
+      case 'text':
+        return $this->text;
+        break;
+      case 'timestamp':
+        return $this->timeStamp;
+        break;
+      default:
+        return $this->text;
+        break;
+    }
+  }
+}
+
+
+?>
--- public_html/messages/index.php
+++ public_html/messages/index.php
@@ -15,7 +15,36 @@ require_once __ROOT__."/inc/html/header.$ioStatus.php";
 // Page begins here, html and body tags are opened in head, closed in footer. Also, main div is closed in footers
 ?>
 
+<div class="column-wrapper">
+    <!--About me-->
+    <div class="column-2">
+      <div class="column-box">
+        <div class="box-padding">
+          <h2 class="h2">Conversations</h2>
+          <?php /*get conversation*/?>
+          <p class="text">
 
+          </p>
+        </div>
+      </div>
+    </div>
+    <!--filters-->
+    <div class="column-2">
+      <div class="column-box">
+        <div class="box-padding">
+          <h2 class="h2">My Details</h2>
+          <?php
+          // retrieve list from database
+          $details = $user->getDetails();
+          foreach ($details as $detail)
+          {
+            echo "$detail <br>";
+          }
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 <?php include __ROOT__."/inc/html/footer.php";?>
