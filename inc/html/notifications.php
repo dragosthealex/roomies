ff951960d41ca5cedb369593ada46a493f4e30c1
mmapzlh9 <liam.higgins@student.manchester.ac.uk>
2015-02-19 01:00:00 +0000

    Added Drop Down Menu, needs changes

--- inc/html/notifications.php
+++ inc/html/notifications.php
@@ -1,3 +1,51 @@
+<!-- message drop -->
+<div class="message-drop" class="drop-hidden">
+      <div class="drop-box">
+        <div class="drop-header">
+          <p class="drop-header-text">Messages</p>
+        </div>
+        <div class="drop-list-wrapper">
+          <div class="drop-list">
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+            <div class="drop-item">
+              <span class="message-icon" style="background-image: url('media/img/anonymous.jpg')"></span>
+              <p class="drop-text"><span class="drop-text-span">Hello my name is profile pic and bal bal ala SF ASDF ASdasf asdf asdgasdf</span></p>
+            </div>
+
+          </div>
+        </div>
+        <div class="drop-footer">
+          <p class="drop-header-text";>View all</p>
+        </div>
+      </div>
+    </div>
 
 <div class="column-wrapper" id="notifications">
   <!--About me-->
