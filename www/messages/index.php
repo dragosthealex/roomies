c81d6f64061d3eb21c0178293ec7cb386f7b5f3f
Daniel Hodgson <daniel.hodgson-2@student.manchester.ac.uk>
2015-02-22 07:32:41 +0000

    Added cookie functionality for whether the user wants to press enter to send.

--- www/messages/index.php
+++ www/messages/index.php
@@ -62,11 +62,18 @@ require_once __ROOT__."/inc/html/header.$ioStatus.php";
         </div>
         <div class="box-padding">
           <!--TODO: CHANGE METHOD TO POST-->
-          <div class="textarea-holder"><textarea class="textarea" id="message" placeholder="Write a message..." oninput="this.style.height=((this.value.match(/\n/g)||[]).length+2)*1.3+'em';return false" onkeydown="return !this.parentNode.nextSibling.firstChild.checked || (event.shiftKey || ((event.keyCode === 13 && this.value.trim()) ? (window.onclick({button:1,target:this.parentNode.nextSibling.nextSibling}), false) : event.keyCode !== 13));"></textarea>
-          </div><label for="pressEnterToSend"><input type="checkbox" id="pressEnterToSend" onchange="var b=this.parentNode.nextSibling,c=b.className;b.className=this.checked?c+'hidden ':c.replace(' hidden ', ' ')" checked> Press Enter to send</label><input type="submit" class="input-button block hidden" value="Send"
-          data-ajax-url="../php/messages.process.php?receiver=<?=$otherUserId?>"
-          data-ajax-post="message"
-          data-ajax-callback="scrollToBottom conv 1">
+          <div class="textarea-holder"
+            ><textarea class="textarea" id="message" placeholder="Write a message..." oninput="this.style.height=((this.value.match(/\n/g)||[]).length+2)*1.3+'em';return false" onkeydown="return !this.parentNode.nextSibling.firstChild.checked || (event.shiftKey || ((event.keyCode === 13 && this.value.trim()) ? (window.onclick({button:1,target:this.parentNode.nextSibling.nextSibling}), false) : event.keyCode !== 13));"></textarea>
+          </div
+          ><label for="pressEnterToSend"
+            ><input type="checkbox" id="pressEnterToSend" onchange="var b=this.parentNode.nextSibling,c=b.className;b.className=this.checked?c+'hidden ':c.replace(' hidden ', ' ');rCookie.set('pressEnterToSend',this.checked,Infinity,'/')">
+            Press Enter to send
+          </label
+          ><input type="submit" class="input-button block " value="Send"
+                  data-ajax-url="../php/messages.process.php?receiver=<?=$otherUserId?>"
+                  data-ajax-post="message"
+                  data-ajax-callback="scrollToBottom conv 1">
+          <script>(function(){var p=document.getElementById('pressEnterToSend');p.checked=rCookie.get('pressEnterToSend')==='true';p.onchange()}())</script>
         </div>
       </div>
     </div>
