4596f93e162f684a0b4a08241ded493a93eb4dc6
Alex Radu <dragos.radu@student.manchester.ac.uk>
2015-03-13 03:40:20 +0000

    improved matching script. now works properly, in any situation, and is foolproof

--- www/php/match.process.php
+++ www/php/match.process.php
@@ -50,12 +50,28 @@ try
   $questionString = implode(":", array("$myAnswer", implode(",", $myAccepted), "$myImportance"));
 
   // Check if row with this user id is existing in ruser_qa
-  $stmt = $con->prepare("INSERT INTO ruser_qa (answer_user_id, question$questionNo) VALUES('$id', '$questionString')
-                          ON DUPLICATE KEY UPDATE question$questionNo='$questionString'");
-  
+  $stmt = $con->prepare("SELECT question$questionNo FROM ruser_qa WHERE EXISTS
+                          (SELECT * FROM ruser_qa WHERE answer_user_id = $id) AND answer_user_id = $id;
+                         INSERT INTO ruser_qa (answer_user_id, question$questionNo) VALUES('$id', '$questionString')
+                           ON DUPLICATE KEY UPDATE question$questionNo='$questionString'");
+
   if(!$stmt->execute())
   {
     throw new Exception("Error updating question $questionNo in database. I shit you not", 1);
+  }
+  $stmt->bindColumn(1, $oldQuestion);
+  $stmt->fetch();
+
+  // Get the old question
+  $oldQuestion = $oldQuestion ? explode(':', $oldQuestion) : array();
+  $oldMyAnswer = isset($oldQuestion[0]) ? $oldQuestion[0] : '';
+  $oldMyAccepted = isset($oldQuestion[1]) ? explode(',', $oldQuestion[1]) : array();
+  $oldMyImportance = isset($oldQuestion[2]) ? $oldQuestion[2] : '';
+  
+  // If it's the same answers, we don't need to update percentages
+  if($oldMyAnswer == $myAnswer && implode(',',$oldMyAccepted) == implode(',', $myAccepted) && $oldMyImportance == $myImportance)
+  {
+    throw new LogicException("Nothing changed", 1);
   }
 
   // Recalculate all percentages
@@ -80,54 +96,68 @@ try
       // Check if we have any errors
       if($otherUser->getError())
       {
-        $errorMsg .= "Error initialising user $id2: ".$otherUser->getError();
+        $response['error'] .= "Error initialising user $id2: ".$otherUser->getError();
         continue;
       }
 
       // Find if the other user has responded to this question
       $otherUserQuestionInfo = $otherUser->getQuestionAnswer($questionNo);
-      $otherAnswer = isset($otherUserQuestionInfo[0])?$otherUserQuestionInfo[0]:'';
+      $otherAnswer = isset($otherUserQuestionInfo[0])?$otherUserQuestionInfo[0]:0;
       $otherAccepted = isset($otherUserQuestionInfo[1])?explode(',', $otherUserQuestionInfo[1]):'';
-      $otherImportance = isset($otherUserQuestionInfo[2])?$otherUserQuestionInfo[2]:'';
+      $otherImportance = isset($otherUserQuestionInfo[2])?$otherUserQuestionInfo[2]:0;
 
       if($otherAnswer && $otherAccepted && $otherImportance)
       {
-        // A new question was answered, so update the max
-        $match['id2_max'] += $myImportance;
+        // Substract the old importance and add the new one
+        $match['id2_max'] = ($match['id2_max'] - $oldMyImportance) + $myImportance;
 
-        // Check if their answer is between my accepted
-        if(in_array($otherAnswer, $myAccepted) && $myImportance != '0')
+        // Check if their answer is between my new accepted but not my old accepted
+        if(in_array($otherAnswer, $myAccepted) && !in_array($otherAnswer, $oldMyAccepted) && $myImportance != '0')
         {
           $match['id2_'.$myImportance]++;
         }
+        // Check if their answar is not in my new accepted but was in my old accepted
+        if(!in_array($otherAnswer, $myAccepted) && in_array($otherAnswer, $oldMyAccepted) && $myImportance != '0' && isset($oldMyAccepted[0]))
+        {
+          $match['id2_'.$myImportance]--;
+        }
+
         // Calculate the new score that represents how much would I like them
         if($match['id2_max'])
         {
           $id2Score = ($match['id2_1'] + $match['id2_10']*10 + $match['id2_50']*50)/$match['id2_max'];
         }
-        //////// NOW FOR ME ///////
 
-        $match['id1_max'] += $otherImportance;
 
-        // Check if their answer is between my accepted
-        if(in_array($myAnswer, $otherAccepted) && $otherImportance != '0')
+        //////// NOW FOR OTHER ///////
+
+        // If it's an already answered question, don't update other's importance, it's the same
+        if(!$oldMyAnswer)
+        {
+          $match['id1_max'] += $otherImportance;
+        }
+
+        // Check if my new answer is between their accepted and my old one is not
+        if(in_array($myAnswer, $otherAccepted) && !in_array($oldMyAnswer, $otherAccepted) && $otherImportance != '0')
         {
           $match['id1_'.$otherImportance]++;
         }
-        if($match['id1_max'])
+        // Check if my new answer is not in their accepted but my old was
+        if(!in_array($myAnswer, $otherAccepted) && in_array($oldMyAnswer, $otherAccepted) && $otherImportance != '0' && $oldMyAnswer)
         {
-          $id1Score = ($match['id1_1'] + $match['id1_10']*10 + $match['id1_50']*50)/$match['id1_max'];
+          $match['id1_'.$otherImportance]--;
         }
 
         // Make sure that neither is 0
-        $id2Score = $id2Score ? $id2Score : 0.001;
-        $id1Score = $id1Score ? $id1Score : 0.001;
-        $percentage = sqrt($id2Score * $id1Score)*100 + 20;
+        $id2Score = isset($id2Score) && $id2Score ? $id2Score : 0.01;
+        $id1Score = isset($id1Score) && $id1Score ? $id1Score : 0.01;
+        $percentage = sqrt($id2Score * $id1Score)*100;
 
         $stmt2 = $con->prepare("UPDATE rpercentages SET id1_1='$match[id1_1]', id1_10='$match[id1_10]', percentage='$percentage',
                                 id1_50='$match[id1_50]', id2_1='$match[id2_1]', id2_10='$match[id2_10]', 
                                 id2_50='$match[id2_50]', id1_max='$match[id1_max]', id2_max='$match[id2_max]'
                                 WHERE percentage_user_id1 = $id AND percentage_user_id2 = $id2");
+        
         if(!$stmt2->execute())
         {
           throw new Exception("Error updating matching details with user $id2", 1);
@@ -155,43 +185,65 @@ try
 
       if($otherAnswer && $otherAccepted && $otherImportance)
       {
-        // A new question was answered, so update the max
-        $match['id1_max'] += $myImportance;
+        // Substract the old importance and add the new one
+        $match['id1_max'] = ($match['id1_max'] - $oldMyImportance) + $myImportance;
 
-        // Check if their answer is between my accepted
-        if(in_array($otherAnswer, $myAccepted) && $myImportance != '0')
+        // Check if their answer is between my new accepted but not my old accepted
+        if(in_array($otherAnswer, $myAccepted) && !in_array($otherAnswer, $oldMyAccepted) && $myImportance != '0')
         {
           $match['id1_'.$myImportance]++;
         }
+        // Check if their answar is not in my new accepted but was in my old accepted
+        if(!in_array($otherAnswer, $myAccepted) && in_array($otherAnswer, $oldMyAccepted) && $myImportance != '0' && isset($oldMyAccepted[0]))
+        {
+          $match['id1_'.$myImportance]--;
+        }
+
+        $response['error'] .= "<br> now ". implode(',',$myAccepted)."   old ".implode(',',$oldMyAccepted) ."<br>";
         // Calculate the new score that represents how much would I like them
         if($match['id1_max'])
         {
           $id1Score = ($match['id1_1'] + $match['id1_10']*10 + $match['id1_50']*50)/$match['id1_max'];
         }
-        //////// NOW FOR OTHER USER ///////
+        //////// NOW FOR MY USER ///////
 
-        $match['id2_max'] += $otherImportance;
+        // If it's an already answered question, don't update other's importance, it's the same
+        if(!$oldMyAnswer)
+        {
+          $match['id2_max'] += $otherImportance;
+        }
 
-        $response['error'] .= "max1 : " . $match['id1_max'] . " max2: " . $match['id2_max'] . "<br>";
-        // Check if their answer is between my accepted
-        if(in_array($myAnswer, $otherAccepted) && $otherImportance != '0')
+        // Check if my new answer is between their accepted and my old one is not
+        if(in_array($myAnswer, $otherAccepted) && !in_array($oldMyAnswer, $otherAccepted) && $otherImportance != '0')
         {
           $match['id2_'.$otherImportance]++;
         }
+        // Check if my new answer is not in their accepted but my old was
+        if(!in_array($myAnswer, $otherAccepted) && in_array($oldMyAnswer, $otherAccepted) && $otherImportance != '0' && $oldMyAnswer)
+        {
+          $match['id2_'.$otherImportance]--;
+        }
+
         if($match['id2_max'])
         {
           $id2Score = ($match['id2_1'] + $match['id2_10']*10 + $match['id2_50']*50)/$match['id2_max'];
         }
 
         // Make sure that neither is 0
-        $id2Score = $id2Score ? $id2Score : 0.001;
-        $id1Score = $id1Score ? $id1Score : 0.001;
-        $percentage = sqrt($id2Score * $id1Score)*100 + 20;
+        $id2Score = isset($id2Score) && $id2Score ? $id2Score : 0.01;
+        $id1Score = isset($id1Score) && $id1Score ? $id1Score : 0.01;
+        $percentage = sqrt($id2Score * $id1Score)*100;
         
         $stmt2 = $con->prepare("UPDATE rpercentages SET id1_1='$match[id1_1]', id1_10='$match[id1_10]', percentage='$percentage',
                                 id1_50='$match[id1_50]', id2_1='$match[id2_1]', id2_10='$match[id2_10]', 
                                 id2_50='$match[id2_50]', id1_max='$match[id1_max]', id2_max='$match[id2_max]'
                                 WHERE percentage_user_id1 = $id2 AND percentage_user_id2 = $id");
+
+        // throw new Exception("UPDATE rpercentages SET id1_1='$match[id1_1]', id1_10='$match[id1_10]', percentage='$percentage',
+        //                 id1_50='$match[id1_50]', id2_1='$match[id2_1]', id2_10='$match[id2_10]', 
+        //                 id2_50='$match[id2_50]', id1_max='$match[id1_max]', id2_max='$match[id2_max]'
+        //                 WHERE percentage_user_id1 = $id AND percentage_user_id2 = $id2", 1);
+
         if(!$stmt2->execute())
         {
           throw new Exception("Error updating matching details with user $id2", 1);
@@ -200,6 +252,10 @@ try
     }// else (if $matchPercentage['user_id1'] == $id)
   }// while (fetch)
 
+  echo json_encode($response);
+}
+catch (LogicException $e)
+{
   echo json_encode($response);
 }
 catch (Exception $e)
