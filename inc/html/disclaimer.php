55f370b26c2f684d9c27750ef4cb7271fe76ad59
mitali.palsikar <mitali.palsikar@student.manchester.ac.uk>
2015-03-13 18:40:21 +0000

    added disclaimer about and terms pages

---
+++ inc/html/disclaimer.php
@@ -0,0 +1,32 @@
+
+<?php
+  $title = "Disclaimer";
+  include '../init.php';
+  require_once __ROOT__."/inc/html/head.php";
+  require_once __ROOT__."/inc/html/header.$ioStatus.php";
+?>
+
+<html>
+  <body class = "body">
+    <div class="column-wrapper">
+      <div class="column-2">
+        <div class="column-box">
+          <div class="box-padding"> 
+            <h2 class = "h2"> Disclaimer </h2>
+            <p class = "text">
+              The information contained in this website is for general information purposes only. The information is provided by <b>findRoomies</b> and while we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
+
+              In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.
+
+              Through this website you are able to link to other websites which are not under the control of <b>findRoomies</b>. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.
+
+              Every effort is made to keep the website up and running smoothly. However, <b>findRoomies</b> takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.
+
+              The template for this disclaimer has been taken from nibusinessinfo.co.uk
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
