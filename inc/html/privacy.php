191107e98b6b95f11107d799a81694ae5b0d6b09
mitali.palsikar <mitali.palsikar@student.manchester.ac.uk>
2015-03-13 19:59:14 +0000

    idgaf2

--- inc/html/privacy.php
+++ inc/html/privacy.php
@@ -4,117 +4,113 @@
   require_once __ROOT__."/inc/html/head.php";
   require_once __ROOT__."/inc/html/header.$ioStatus.php";
 ?>
-<html>
-  <body class = "body">
-    <div class="column-wrapper">
-      <div class="column-2">
-        <div class="column-box">
-          <div class="box-padding"> 
-            <h2 class = "h2"> About </h2>
-            <p class = "text">
-              <b> What is this Privacy Policy for?</b>
+<div class="column-wrapper">
+  <div class="column-2">
+    <div class="column-box">
+      <div class="box-padding"> 
+        <h2 class = "h2"> About </h2>
+        <p class = "text">
+          <b> What is this Privacy Policy for?</b>
 
-              This privacy policy is for this website [www.findroomies.co.uk] and governs the
-              privacy of its users who choose to use it.
+          This privacy policy is for this website [www.findroomies.co.uk] and governs the
+          privacy of its users who choose to use it.
 
-              The policy sets out the different areas where user privacy is concerned and outlines
-              the obligations & requirements of the users, the website and website owners.
-              Furthermore the way this website processes, stores and protects user data and
-              information will also be detailed within this policy.
+          The policy sets out the different areas where user privacy is concerned and outlines
+          the obligations & requirements of the users, the website and website owners.
+          Furthermore the way this website processes, stores and protects user data and
+          information will also be detailed within this policy.
 
-              <b>The Website</b>
+          <b>The Website</b>
 
-              This website and its owners take a proactive approach to user privacy and ensure the
-              necessary steps are taken to protect the privacy of its users throughout their
-              visiting experience. This website comply's to all UK national laws and requirements
-              for user privacy.
+          This website and its owners take a proactive approach to user privacy and ensure the
+          necessary steps are taken to protect the privacy of its users throughout their
+          visiting experience. This website comply's to all UK national laws and requirements
+          for user privacy.
 
-              <b>Use of Cookies</b>
+          <b>Use of Cookies</b>
 
-              This website uses cookies to better the users experience while visiting the website.
-              Where applicable this website uses a cookie control system allowing the user on their
-              first visit to the website to allow or disallow the use of cookies on their computer /
-              device. This complies with recent legislation requirements for websites to obtain
-              explicit consent from users before leaving behind or reading files such as cookies on
-              a user's computer / device. Cookies are small files saved to the user's computers hard
-              drive that track, save and store information about the user's interactions and usage
-              of the website. This allows the website, through its server to provide the users with
-              a tailored experience within this website.
+          This website uses cookies to better the users experience while visiting the website.
+          Where applicable this website uses a cookie control system allowing the user on their
+          first visit to the website to allow or disallow the use of cookies on their computer /
+          device. This complies with recent legislation requirements for websites to obtain
+          explicit consent from users before leaving behind or reading files such as cookies on
+          a user's computer / device. Cookies are small files saved to the user's computers hard
+          drive that track, save and store information about the user's interactions and usage
+          of the website. This allows the website, through its server to provide the users with
+          a tailored experience within this website.
 
-              Users are advised that if they wish to deny the use and saving of cookies from this
-              website on to their computers hard drive they should take necessary steps within their
-              web browsers security settings to block all cookies from this website and its external
-              serving vendors.
+          Users are advised that if they wish to deny the use and saving of cookies from this
+          website on to their computers hard drive they should take necessary steps within their
+          web browsers security settings to block all cookies from this website and its external
+          serving vendors.
 
-              This website uses tracking software to monitor its visitors to better understand how
-              they use it. This software is provided by Google Analytics which uses cookies to track
-              visitor usage. The software will save a cookie to your computers hard drive in order
-              to track and monitor your engagement and usage of the website, but will not store,
-              save or collect personal information. You can read Google's privacy policy here for
-              further information [ http://www.google.com/privacy.html ].
+          This website uses tracking software to monitor its visitors to better understand how
+          they use it. This software is provided by Google Analytics which uses cookies to track
+          visitor usage. The software will save a cookie to your computers hard drive in order
+          to track and monitor your engagement and usage of the website, but will not store,
+          save or collect personal information. You can read Google's privacy policy here for
+          further information [ http://www.google.com/privacy.html ].
 
-              Other cookies may be stored to your computers hard drive by external vendors when this
-              website uses referral programs, sponsored links or adverts. Such cookies are used for
-              conversion and referral tracking and typically expire after 30 days, though some may
-              take longer. No personal information is stored, saved or collected.
+          Other cookies may be stored to your computers hard drive by external vendors when this
+          website uses referral programs, sponsored links or adverts. Such cookies are used for
+          conversion and referral tracking and typically expire after 30 days, though some may
+          take longer. No personal information is stored, saved or collected.
 
-              <b>Contact & Communication</b>               
-              Users contacting this website and/or its
-              owners do so at their own discretion and provide any such personal details requested at their own risk. Your personal information is kept private and stored securely until a time it is no longer required or has no use, as detailed in the Data Protection Act 1998. Every effort has been made to ensure a safe and secure form to email submission process but advise users using such form to email processes that they do so at their own risk.               
+          <b>Contact & Communication</b>               
+          Users contacting this website and/or its
+          owners do so at their own discretion and provide any such personal details requested at their own risk. Your personal information is kept private and stored securely until a time it is no longer required or has no use, as detailed in the Data Protection Act 1998. Every effort has been made to ensure a safe and secure form to email submission process but advise users using such form to email processes that they do so at their own risk.               
 
-              This website and its owners use anyinformation submitted to provide you with further information about the products / services to offer or to assist you in answering any questions or queries you may have submitted. This includes using your details to subscribe you to any email newsletter program the website operates but only if
-              this was made clear to you and your express permission was granted when submitting any form to email
-              process. Or whereby you the consumer have previously purchased from or enquired about purchasing
-              from the company a product or service that the email newsletter relates to. This is by no means an
-              entire list of your user rights in regard to receiving email marketing material. Your details are
-              not passed on to any third parties.
+          This website and its owners use anyinformation submitted to provide you with further information about the products / services to offer or to assist you in answering any questions or queries you may have submitted. This includes using your details to subscribe you to any email newsletter program the website operates but only if
+          this was made clear to you and your express permission was granted when submitting any form to email
+          process. Or whereby you the consumer have previously purchased from or enquired about purchasing
+          from the company a product or service that the email newsletter relates to. This is by no means an
+          entire list of your user rights in regard to receiving email marketing material. Your details are
+          not passed on to any third parties.
 
-              
-              <b> External Links</b>               
-              Although this website only looks to include quality, safe and relevant external links, users are advised adopt a policy of caution before clicking any external web links mentioned throughout this website. (External links are clickable text / banner / image links to other websites, similar to; www.creamside.co.uk and Pembrokeshire
-              Windows this.)    
+          
+          <b> External Links</b>               
+          Although this website only looks to include quality, safe and relevant external links, users are advised adopt a policy of caution before clicking any external web links mentioned throughout this website. (External links are clickable text / banner / image links to other websites, similar to; www.creamside.co.uk and Pembrokeshire
+          Windows this.)    
 
-              The owners of this website cannot guarantee or verify the contents of any externally linked website despite their best efforts. Users should therefore note they click on
-              external links at their own risk and this website and its owners cannot be held liable for any damages or implications caused by visiting any external links mentioned.
+          The owners of this website cannot guarantee or verify the contents of any externally linked website despite their best efforts. Users should therefore note they click on
+          external links at their own risk and this website and its owners cannot be held liable for any damages or implications caused by visiting any external links mentioned.
 
-              <b>Adverts and Sponsored Links</b>               
-              This website may contain sponsored links and adverts. These will typically be served through our advertising partners, to whom may have detailed privacy policies relating directly to the adverts they serve.               
+          <b>Adverts and Sponsored Links</b>               
+          This website may contain sponsored links and adverts. These will typically be served through our advertising partners, to whom may have detailed privacy policies relating directly to the adverts they serve.               
 
-              Clicking on any such adverts will send you to the advertisers website through a referral program which may use cookies and will track the number of referrals sent from this website. This may include the use of cookies which may in turn be saved on your computers hard drive. Users should therefore note they click on sponsored external links at their own risk and this website and its owners cannot be held liable for any damages or implications caused by visiting any external links mentioned.
+          Clicking on any such adverts will send you to the advertisers website through a referral program which may use cookies and will track the number of referrals sent from this website. This may include the use of cookies which may in turn be saved on your computers hard drive. Users should therefore note they click on sponsored external links at their own risk and this website and its owners cannot be held liable for any damages or implications caused by visiting any external links mentioned.
 
-              <b>Social Media Platforms</b>               
+          <b>Social Media Platforms</b>               
 
-              Communication, engagement and actions taken through external social media platforms that this website and its owners participate on are custom to the terms and conditions as well as the privacy policies held with each social media
-              platform respectively.               
+          Communication, engagement and actions taken through external social media platforms that this website and its owners participate on are custom to the terms and conditions as well as the privacy policies held with each social media
+          platform respectively.               
 
-              Users are advised to use social media platforms wisely and communicate / engage upon them with due care and caution in regard to their own privacy and personal
-              details. This website nor its owners will ever ask for personal or sensitive information through social media platforms and encourage users wishing to discuss sensitive details to contact them through primary communication channels such as by telephone or email.               
+          Users are advised to use social media platforms wisely and communicate / engage upon them with due care and caution in regard to their own privacy and personal
+          details. This website nor its owners will ever ask for personal or sensitive information through social media platforms and encourage users wishing to discuss sensitive details to contact them through primary communication channels such as by telephone or email.               
 
-              This website may use social sharing buttons which help share web content directly from web pages to the social media platform in question. Users are advised before using such social sharing buttons that they do so at their own discretion and note that the social media platform may track and save your request to share a web page respectively through your social media platform account.
+          This website may use social sharing buttons which help share web content directly from web pages to the social media platform in question. Users are advised before using such social sharing buttons that they do so at their own discretion and note that the social media platform may track and save your request to share a web page respectively through your social media platform account.
 
-              <b>Shortened Links in Social Media</b>               
-              This website and its owners through their social media platform accounts may share web links to relevant web pages. By default some social media platforms shorten lengthy urls [web addresses] (this is an example:
-              http://bit.ly/zyVUBo).                
+          <b>Shortened Links in Social Media</b>               
+          This website and its owners through their social media platform accounts may share web links to relevant web pages. By default some social media platforms shorten lengthy urls [web addresses] (this is an example:
+          http://bit.ly/zyVUBo).                
 
-              Users are advised to take caution and good judgement before clicking any shortened urls published on social media platforms by this website and its owners.Despite the best efforts to ensure only genuine urls are published many social media platforms are prone to spam and hacking and therefore this website and its owners cannot be held liable for any damages or implications caused by visiting any shortened links.
-            
-              <b>Resources & Further Information</b>
-              Data Protection Act 1998
-              Privacy and Electronic Communications Regulations 2003
-              Privacy and Electronic Communications Regulations 2003 - The Guide
-              Twitter Privacy Policy
-              Facebook Privacy Policy
-              Google Privacy Policy
-              Linkedin Privacy Policy
-              Mailchimp Privacy Policy
-              Web Design Pembrokeshire
-              Website Privacy Policy
-              Project Planner Template
-              v.2.0 April 2013 Edited & customised by: findRoomies
-              </p>
-          </div>
-        </div>
+          Users are advised to take caution and good judgement before clicking any shortened urls published on social media platforms by this website and its owners.Despite the best efforts to ensure only genuine urls are published many social media platforms are prone to spam and hacking and therefore this website and its owners cannot be held liable for any damages or implications caused by visiting any shortened links.
+        
+          <b>Resources & Further Information</b>
+          Data Protection Act 1998
+          Privacy and Electronic Communications Regulations 2003
+          Privacy and Electronic Communications Regulations 2003 - The Guide
+          Twitter Privacy Policy
+          Facebook Privacy Policy
+          Google Privacy Policy
+          Linkedin Privacy Policy
+          Mailchimp Privacy Policy
+          Web Design Pembrokeshire
+          Website Privacy Policy
+          Project Planner Template
+          v.2.0 April 2013 Edited & customised by: findRoomies
+          </p>
       </div>
     </div>
-  </body>
-</html>
+  </div>
+</div>
