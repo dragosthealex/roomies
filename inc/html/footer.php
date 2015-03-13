962c4d81d3b3d749bd9ea12f854341dc38940a3e
mitali.palsikar <mitali.palsikar@student.manchester.ac.uk>
2015-03-13 19:09:09 +0000

    added privacy and updated footer pages

--- inc/html/footer.php
+++ inc/html/footer.php
@@ -10,10 +10,10 @@ For the sake of layout, let the body/html be closed on every page
 <div class="box">
 	<ul class="box-padding footer">
 		<li class="footer-item">Roomies &copy; 2014</li>
-		<li class="footer-item"><a href="/roomies/inc/html/about.php" class="footer-link">About</a></li>
-		<li class="footer-item"><a href="/roomies/inc/html/Terms.php" class="footer-link">Terms</a></li>
-		<li class="footer-item"><a href="#" class="footer-link">Privacy</a></li>
-        <li class="footer-item"><a href="/roomies/inc/html/disclaimer.php" class="footer-link">Disclaimer</a></li>
+		<li class="footer-item"><a href="about.php" class="footer-link">About</a></li>
+		<li class="footer-item"><a href="terms.php" class="footer-link">Terms</a></li>
+		<li class="footer-item"><a href="privacy.php" class="footer-link">Privacy</a></li>
+        <li class="footer-item"><a href="disclaimer.php" class="footer-link">Disclaimer</a></li>
 		<li class="footer-item"><a href="#" class="footer-link">Cookies</a></li>
 	</ul>
 </div>
