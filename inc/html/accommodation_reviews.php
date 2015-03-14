a13f1ccec9b142f9272637382b98c309cc6ec207
mmapzlh9 <liam.higgins@student.manchester.ac.uk>
2015-03-14 19:45:49 +0000

    bleu

--- inc/html/accommodation_reviews.php
+++ inc/html/accommodation_reviews.php
@@ -24,26 +24,39 @@
               <div class="like-reply">
                 Like - Reply
               </div>
-              <div class="reply-box">
                 <div class="review-header" style="border-top: 1px solid #d5d1d0; padding-top: 5px; text-align: right; padding-right: 10px;">
                   <a class="click-me" onclick="toggleReply(this)">Replies</a>
                 </div>
-                <div class="reply" id="hide">
+                <div class="reply-box">
+                  <div class="reply" id="hide">
+                      <div class="author-details">
+                        <div class="reply-text">
+                          <div class="reply-pic">
 
-                    <div class="author-details">
-
-                      <div class="reply-text">
-                        <div class="reply-pic">
-
+                          </div>
+                          <a class="link">Alex Radu</a> - The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                         </div>
-                        <a class="link">Alex Radu</a> - The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                       </div>
-                    </div>
+                  </div>
+                  <div class="reply" id="hide">
+                      <div class="author-details">
+                        <div class="reply-text">
+                          <div class="reply-pic">
+
+                          </div>
+                          <a class="link">Alex Radu</a> - The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                        </div>
+                      </div>
+                  </div>
                 </div>
-              </div>
+
             </div>
 <script type="text/javascript">
-  document.getElementById('hide').style.display = 'none';
+  var hideAll = document.getElementsByClassName('reply-box');
+  for(i = 0; i < hideAll.length; i ++)
+  {
+    hideAll[i].style.display = 'none';
+  }
 
   function toggleReply(param) {
     var parent = param.parentNode;
