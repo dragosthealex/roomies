7a74267d5162222d722ebbb5ef70144ea2535067
mmapzlh9 <liam.higgins@student.manchester.ac.uk>
2015-03-14 04:37:05 +0000

    Finished css and html, Need to add the loop for the reviews, and populate database

--- inc/html/accommodation_reviews.php
+++ inc/html/accommodation_reviews.php
@@ -3,47 +3,42 @@
 
 // $replyText = $reviews[$y]['replies'][$x]['text'];
 ?>
-<div class="review-box">
-            <div class="review-header">
-              Reviews
-            </div>
-            <div class="author-details">
-                <div class="profile-review">
+          <div class="review-header">Reviews</div>
+            <div class="review-box">
+              <div class="author-details">
+                  <div class="review-pic">
 
-                </div>
-                <div class="author-text">
-                  <div class="author-name">
-                    Liam Higgins
                   </div>
-                  <div class="date-text">
-                    27 February
+                  <div class="author-text">
+                    <div class="author-name">
+                      <a class="link">Liam Higgins</a>
+                    </div>
+                    <div class="date-text">
+                      27 February
+                    </div>
                   </div>
-                </div>
-            </div>
-            <div class="review-text">
-              The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-            </div>
-            <div class="date-text">
-              Like - Reply
-            </div>
-            <div class="like-reply">
-
-            </div>
-            <div class="reply-box">
+              </div>
+              <div class="review-text">
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+              </div>
+              <div class="like-reply">
+                Like - Reply
+              </div>
+              <div class="reply-box">
                 <div class="review-header" style="border-top: 1px solid #d5d1d0; padding-top: 5px; text-align: right; padding-right: 10px;">
                   Replies
                 </div>
                 <div class="reply">
 
                     <div class="author-details">
-                        <div class="profile-review">
+
+                      <div class="reply-text">
+                        <div class="reply-pic">
 
                         </div>
-                      <div class="reply-text">
-                            <a class="author-link">Alex Radu</a> - The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                        <a class="link">Alex Radu</a> - The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
                       </div>
                     </div>
-
                 </div>
               </div>
             </div>
