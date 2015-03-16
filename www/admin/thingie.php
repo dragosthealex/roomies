b128d53aba8ab02b9d66d8a3772dbbb14199cec3
Alex Radu <dragos.radu@student.manchester.ac.uk>
2015-03-16 18:20:06 +0000

    added more details columns in db

---
+++ www/admin/thingie.php
@@ -0,0 +1,91 @@
+<?php
+
+
+$ethnicity = array (
+            "Asian",
+            "Native American",
+            "Hispanic / Latin",
+            "Middle Eastern",
+            "Indian",
+            "White Master Race",
+            "Black / nigga",
+            "Pacific Islander (just fucking Pacific)",
+            "Other",
+            "Other2, for those not in Other");
+
+$smokes = array (
+            "Yes",
+            "Sometimes",
+            "When drinking",
+            "Trying to quit",
+            "No");
+
+$drinks = array(
+            "Very often",
+            "Often",
+            "Socially",
+            "Rarely",
+            "Desperately",
+            "Not at all");
+
+$drugs = array (
+            "Often",
+            "Sometimes",
+            "Never");
+
+$studies = array (
+            "Very Often",
+            "Often",
+            "Sometimes",
+            "Rarely",
+            "Never");
+$degree = array (
+            "Accounting, Business & Finance",
+            "Agriculture and Horticulture",
+            "Archaeology",
+            "Architecture, Building & Planning",
+            "Art and design",
+            "Biology",
+            "Chemistry",
+            "Communication and Media",
+            "Computing & IT",
+            "Dentistry",
+            "Earth Sciences",
+            "Economics",
+            "Education",
+            "Engineering",
+            "English Language",
+            "English Literature",
+            "Fashion and textiles",
+            "Geography",
+            "Health and Medicine",
+            "History",
+            "Hospitality & Catering",
+            "Languages",
+            "Law",
+            "Management",
+            "Marketing",
+            "Mathematics",
+            "Music",
+            "Nursing",
+            "Pharmacology",
+            "Philosophy",
+            "Physics",
+            "Politics",
+            "Psychology and Counselling",
+            "Social Work",
+            "Sociology",
+            "Sports & Leisure",
+            "Theatre & Dramatic Arts",
+            "Theology & Religion",
+            "Travel and Tourism",
+            "Veterinary Medicine");
+
+$offspring = array (
+            "Has a kid",
+            "Has kids",
+            "Does not have kids",
+            "Eats kids");
+
+$pets = array (
+            );
