14731e29350bd6c1a5dc34000a27449068e6fb7e
Alex Radu <dragos.radu@student.manchester.ac.uk>
2015-04-20 14:02:02 +0100

    stuff3

--- www/admin/reset_db.php
+++ www/admin/reset_db.php
@@ -1,21 +1,10 @@
 <?php
 require_once '../../inc/init.php';
 
-$stmt = $con->prepare("DELETE FROM rusers");
-$stmt->execute();
-$stmt = $con->prepare("DELETE FROM ruser_qa");
-$stmt->execute();
-$stmt = $con->prepare("DELETE FROM rusersettings");
-$stmt->execute();
-$stmt = $con->prepare("DELETE FROM rgroups");
-$stmt->execute();
-$stmt = $con->prepare("DELETE FROM rdetails");
-$stmt->execute();
-$stmt = $con->prepare("DELETE FROM rconexions");
-$stmt->execute();
-$stmt = $con->prepare("DELETE FROM raccommodations");
-$stmt->execute();
-$stmt = $con->prepare("DELETE FROM rowners");
-$stmt->execute();
-
+$stable = array('rusers', 'ruser_qa', 'rusersettings', 'rgroups', 'rdetails', 'rconexions', 'raccommodations', 'rowners', 'rposts');
+foreach ($stable as $horse)
+{
+  $stmt = $con->prepare("DELETE FROM $horse; ALTER TABLE $horse AUTO_INCREMENT = 1");
+  $stmt->execute();
+}
 ?>
